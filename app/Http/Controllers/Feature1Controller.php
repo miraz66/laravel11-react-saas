d00e9bd4cf18696600744a5b76c61740ef593ed2
Mirajul Islam <mirajulislam0509@gmail.com>
2024-05-25 10:23:39 +0600

    Create a Freature controller

--- app/Http/Controllers/Feature1Controller.php
+++ app/Http/Controllers/Feature1Controller.php
@@ -2,9 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Feature;
 use Illuminate\Http\Request;
 
 class Feature1Controller extends Controller
 {
-    //
+    public ?Feature $feature = null;
+
+    public function __construct() {}
+
+    public function index(Request $request) {}
+
+    public function calculate(Request $request) {}
 }
