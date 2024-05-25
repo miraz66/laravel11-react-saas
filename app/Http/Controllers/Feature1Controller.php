cd57da4737b7946665f2dfee344e104a98e96506
Mirajul Islam <mirajulislam0509@gmail.com>
2024-05-25 11:07:30 +0600

    Create a Freature controller

--- app/Http/Controllers/Feature1Controller.php
+++ app/Http/Controllers/Feature1Controller.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Feature;
 use Illuminate\Http\Request;
 use App\Http\Resources\FeatureResource;
+use App\Models\UsedFeature;
 
 class Feature1Controller extends Controller
 {
@@ -25,6 +26,27 @@ class Feature1Controller extends Controller
 
     public function calculate(Request $request)
     {
-        
+        $user = $request->user();
+
+        if($user->available_credits < $this->feature->required_credits) {
+            return back();
+        }
+        $data = $request-> validate([
+            'number1' => ['required', 'numeric'],
+            'number2' => ['required', 'numeric'],
+        ]);
+        $number1 = (float) $data['number1'];
+        $number2 = (float) $data['number2'];
+
+        $user->decreaseCredits($this->feature->required_credits);
+
+        UsedFeature::create([
+            'feature1' => $this->feature->id,
+            'user_id' => $user->id,
+            'credits' => $this->feature->required_credits,
+            'data' => $data
+        ]);
+
+        return to_route('feature1.index')->with('answer', $number1 + $number2);
     }
 }
