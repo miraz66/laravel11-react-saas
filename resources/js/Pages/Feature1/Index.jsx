8e1e43af2907f4f3fef7ca0808e2af02369e8a64
Mirajul Islam <mirajulislam0509@gmail.com>
2024-05-28 22:45:41 +0600

    Set Index

--- resources/js/Pages/Feature1/Index.jsx
+++ resources/js/Pages/Feature1/Index.jsx
@@ -14,6 +14,40 @@ export default function Index({ feature, answer }) {
     const submit = (e) => {
         e.preventDefault();
 
-        post(route("features", feature.id));
+        post(route("feature1.calculate"), {
+            onSuccess() {
+                reset();
+            },
+        });
     };
+
+    return (
+        <Feature feature={feature} answer={answer}>
+            <form className="p-8 grid grid-cols-2 gap-3" onSubmit={submit}>
+                <InputLabel htmlFor="number1" value="Number 1" />
+                <TextInput
+                    id="number1"
+                    className="mt-1 block w-full"
+                    value={data.number1}
+                    onChange={(e) => setData("number1", e.target.value)}
+                    required
+                    isFocused
+                    autoComplete="number1"
+                />
+                <InputError error={errors.number1} />
+
+                <InputLabel htmlFor="number2" value="Number 2" />
+                <TextInput
+                    id="number2"
+                    className="mt-1 block w-full"
+                    value={data.number2}
+                    onChange={(e) => setData("number2", e.target.value)}
+                    required
+                    isFocused
+                    autoComplete="number2"
+                />
+                <InputError error={errors.number2} />
+            </form>
+        </Feature>
+    );
 }
